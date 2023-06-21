feeb25d372d44779cf3a104d0f07cde3cd4d551c
Mohammed Fakih <mhd.fakih@gmail.com>
2023-06-21 14:14:47 +0300

    final solution

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 "use strict";
 
-const usersRatingArray = document.querySelectorAll(".rating-number");
-const userRatingDiv = document.querySelectorAll(".rating");
+const usersRatingArray = Array.from(
+  document.querySelectorAll(".rating-number")
+);
+const userRatingDiv = Array.from(document.querySelectorAll(".rating"));
 const submitButton = document.querySelector(".btn");
 const userRatingInput = document.querySelector(".rating-input");
 
-function submitRating() {
-  console.log("hello world");
+
+// Remove Active state from all elements
+function removeActiveClass() {
+  userRatingDiv.forEach(function (element) {
+    element.classList.remove("active");
+  });
 }
 
-submitButton.addEventListener("click", submitRating);
+// clear the rate selection when pressing ESC key
+document.addEventListener("keydown", (event) => {
+  if (event.key === 27) {
+    return;
+  }
+  removeActiveClass();
+  userRatingInput.value = null;
+  submitButton.setAttribute("disabled", "disabled");
+});
+
+
+// set active class on selected rate element in DOM
+for (let i = 0; i < userRatingDiv.length; i++) {
+  userRatingDiv[i].onclick = function () {
+    removeActiveClass();
+    this.classList.add("active");
+    userRatingInput.value = this.textContent;
+    submitButton.removeAttribute("disabled");
+  };
+}
